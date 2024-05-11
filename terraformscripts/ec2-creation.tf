1b7a95c604a60cb6e812fecd00fb1831437cc902
syampavan6806 <139321405+syampavan6806@users.noreply.github.com>
2024-05-11 21:50:07 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-region = ap-south-1
+region = "ap-south-1"
 }
 resource "aws_instance" "AWSInstance" {
 count = 1
 ami = "ami-013e83f579886baeb"
 instance_type = "t2.micro"
 key_name = "syampawan"
-security_groups = [launch-wizard-3]
+security_groups = ["launch-wizard-3"]
 tags={
 Name = "tomcatserver"
 }
