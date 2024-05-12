8222027ca0231e6401c778bb4c58cc0f066e99f5
syampavan6806 <139321405+syampavan6806@users.noreply.github.com>
2024-05-12 12:33:24 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 }
 resource "aws_instance" "AWSInstance" {
 count = 1
-ami = "ami-013e83f579886baeb"
+ami = "ami-013e83f579886bae"
 instance_type = "t2.micro"
 key_name = "syampawan"
 security_groups = ["launch-wizard-3"]
